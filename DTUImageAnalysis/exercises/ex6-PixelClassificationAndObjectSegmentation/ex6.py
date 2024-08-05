2c50186f8d1075402bfc95976c88b6fc1f029efb
madsr2d2 <mads.richardt@gmail.com>
2024-08-05 12:11:20 +0200

    mads

--- DTUImageAnalysis/exercises/ex6-PixelClassificationAndObjectSegmentation/ex6.py
+++ DTUImageAnalysis/exercises/ex6-PixelClassificationAndObjectSegmentation/ex6.py
@@ -12,6 +12,7 @@ from scipy.spatial import distance
 
 def show_comparison(original, modified, modified_name):
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8, 4), sharex=True, sharey=True)
+
     ax1.imshow(original, cmap="gray", vmin=-200, vmax=500)
     ax1.set_title("Original")
     ax1.axis("off")
@@ -19,3 +20,21 @@ def show_comparison(original, modified, modified_name):
     ax2.set_title(modified_name)
     ax2.axis("off")
     io.show()
+
+
+dataDir = "data/"
+ct = dicom.read_file(dataDir + "Training.dcm")
+img = ct.pixel_array
+print(img.shape)
+print(img.dtype)
+
+io.imshow(img, vmin=0, vmax=150, cmap="gray")
+io.show()
+
+spleen_roi = io.imread(dataDir + "SpleenROI.png")
+# convert to boolean image
+spleen_mask = spleen_roi > 0
+spleen_values = img[spleen_mask]
+meanValus = spleen_values.mean()
+spleen_valuesSD = spleen_values.std()
+print(f"mean: {meanValus}, SD: {spleen_valuesSD}")
